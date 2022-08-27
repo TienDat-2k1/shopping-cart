f45e142f05b5a72793a0c8bcec5b8c6639a6cbcd
Tien Dat <tonguyentiendat@gmail.com>
2022-08-27 13:42:19 +0700

    react and typescript shopping cart

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,50 @@
+import { Button, Stack } from 'react-bootstrap';
+import {
+  CartItem as CartItemProps,
+  useShoppingCart,
+} from '../context/ShoppingCartContext';
+import storeItems from '../data/items.json';
+import { formatCurrency } from '../utilities/formatCurrency';
+
+const CartItem = ({ id, quantity, ...otherProps }: CartItemProps) => {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find(i => i.id === id);
+  if (item == null) return null;
+
+  return (
+    <Stack direction="horizontal" gap={2}>
+      <img
+        src={item.imgUrl}
+        style={{ width: '125px', height: '75px', objectFit: 'cover' }}
+        alt=""
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{' '}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: '.65rem' }}>
+              x {quantity}
+            </span>
+          )}
+        </div>
+        <div
+          className="text-muted"
+          style={{
+            fontSize: '.75rem',
+          }}
+        >
+          {formatCurrency(item.price)}
+        </div>
+        <div>{formatCurrency(item.price * quantity)}</div>
+        <Button
+          variant="outline-danger"
+          size="sm"
+          onClick={() => removeFromCart(item.id)}
+        >
+          &times;
+        </Button>
+      </div>
+    </Stack>
+  );
+};
+export default CartItem;
